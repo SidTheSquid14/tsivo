146b6204dc5cb342423bfe7091132b349d2b6052
SidTheSquid14 <48692433+SidTheSquid14@users.noreply.github.com>
2019-03-18 13:24:43 -0500

    Add files via upload

---
+++ dist/js/main.js
@@ -0,0 +1,250 @@
+function checkAnswer() {
+// Set the vaue of the underlined word
+partOfSpeech = 'adjective';
+
+// Get the user's input value
+userAnswer = document.getElementById('userAnswer').value;
+
+// Compare the user's answer to the correct answer
+if (partOfSpeech == userAnswer) {
+    // Log a message to make sure your logic works
+    console.log('They got it right!');
+
+    // Flash a message to the user letting them know it's right
+    message = document.getElementById('message');
+    message.innerHTML = 'That is the correct answer!';
+
+} else {
+    // Log a message to make sure your logic works
+    console.log('Incorrect');
+
+    // Flash a message to the user letting them know it's wrong
+    message = document.getElementById('message');
+    message.innerHTML = 'That is unfortunate.';
+  }
+}
+function checkAnswer2() {
+// Set the vaue of the underlined word
+partOfSpeech2 = 'verb';
+
+// Get the user's input value
+userAnswer2 = document.getElementById('userAnswer2').value;
+
+// Compare the user's answer to the correct answer
+if (partOfSpeech2 == userAnswer2) {
+    // Log a message to make sure your logic works
+    console.log('They got it right!');
+
+    // Flash a message to the user letting them know it's right
+    message2 = document.getElementById('message2');
+    message2.innerHTML = 'That is the correct answer!';
+
+} else {
+    // Log a message to make sure your logic works
+    console.log('Incorrect');
+
+    // Flash a message to the user letting them know it's wrong
+    message2 = document.getElementById('message2');
+    message2.innerHTML = 'That is unfortunate.';
+  }
+}
+function checkAnswer3() {
+// Set the vaue of the underlined word
+partOfSpeech3 = 'pronoun';
+
+// Get the user's input value
+userAnswer3 = document.getElementById('userAnswer3').value;
+
+// Compare the user's answer to the correct answer
+if (partOfSpeech3 == userAnswer3) {
+    // Log a message to make sure your logic works
+    console.log('They got it right!');
+
+    // Flash a message to the user letting them know it's right
+    message3 = document.getElementById('message3');
+    message3.innerHTML = 'That is the correct answer!';
+
+} else {
+    // Log a message to make sure your logic works
+    console.log('Incorrect');
+
+    // Flash a message to the user letting them know it's wrong
+    message3 = document.getElementById('message3');
+    message3.innerHTML = 'That is unfortunate.';
+  }
+}
+function checkAnswer4() {
+// Set the vaue of the underlined word
+partOfSpeech4 = 'interjection';
+
+// Get the user's input value
+userAnswer4 = document.getElementById('userAnswer4').value;
+
+// Compare the user's answer to the correct answer
+if (partOfSpeech4 == userAnswer4) {
+    // Log a message to make sure your logic works
+    console.log('They got it right!');
+
+    // Flash a message to the user letting them know it's right
+    message4 = document.getElementById('message4');
+    message4.innerHTML = 'That is the correct answer!';
+
+} else {
+    // Log a message to make sure your logic works
+    console.log('Incorrect');
+
+    // Flash a message to the user letting them know it's wrong
+    message4 = document.getElementById('message4');
+    message4.innerHTML = 'That is unfortunate.';
+  }
+}
+function checkAnswer5() {
+// Set the vaue of the underlined word
+partOfSpeech5 = 'adverb';
+
+// Get the user's input value
+userAnswer5 = document.getElementById('userAnswer5').value;
+
+// Compare the user's answer to the correct answer
+if (partOfSpeech5 == userAnswer5) {
+    // Log a message to make sure your logic works
+    console.log('They got it right!');
+
+    // Flash a message to the user letting them know it's right
+    message5 = document.getElementById('message5');
+    message5.innerHTML = 'That is the correct answer!';
+
+} else {
+    // Log a message to make sure your logic works
+    console.log('Incorrect');
+
+    // Flash a message to the user letting them know it's wrong
+    message5 = document.getElementById('message5');
+    message5.innerHTML = 'That is unfortunate.';
+  }
+}
+function checkAnswer6() {
+// Set the vaue of the underlined word
+partOfSpeech6 = 'adjective';
+
+// Get the user's input value
+userAnswer6 = document.getElementById('userAnswer6').value;
+
+// Compare the user's answer to the correct answer
+if (partOfSpeech6 == userAnswer6) {
+    // Log a message to make sure your logic works
+    console.log('They got it right!');
+
+    // Flash a message to the user letting them know it's right
+    message6 = document.getElementById('message6');
+    message6.innerHTML = 'That is the correct answer!';
+
+} else {
+    // Log a message to make sure your logic works
+    console.log('Incorrect');
+
+    // Flash a message to the user letting them know it's wrong
+    message6 = document.getElementById('message6');
+    message6.innerHTML = 'That is unfortunate.';
+  }
+}
+function checkAnswer7() {
+// Set the vaue of the underlined word
+partOfSpeech7 = 'noun';
+
+// Get the user's input value
+userAnswer7 = document.getElementById('userAnswer7').value;
+
+// Compare the user's answer to the correct answer
+if (partOfSpeech7 == userAnswer7) {
+    // Log a message to make sure your logic works
+    console.log('They got it right!');
+
+    // Flash a message to the user letting them know it's right
+    message7 = document.getElementById('message7');
+    message7.innerHTML = 'That is the correct answer!';
+
+} else {
+    // Log a message to make sure your logic works
+    console.log('Incorrect');
+
+    // Flash a message to the user letting them know it's wrong
+    message7 = document.getElementById('message7');
+    message7.innerHTML = 'That is unfortunate.';
+  }
+}
+function checkAnswer8() {
+// Set the vaue of the underlined word
+partOfSpeech8 = 'verb';
+
+// Get the user's input value
+userAnswer8 = document.getElementById('userAnswer8').value;
+
+// Compare the user's answer to the correct answer
+if (partOfSpeech8 == userAnswer8) {
+    // Log a message to make sure your logic works
+    console.log('They got it right!');
+
+    // Flash a message to the user letting them know it's right
+    message8 = document.getElementById('message8');
+    message8.innerHTML = 'That is the correct answer!';
+
+} else {
+    // Log a message to make sure your logic works
+    console.log('Incorrect');
+
+    // Flash a message to the user letting them know it's wrong
+    message8 = document.getElementById('message8');
+    message8.innerHTML = 'That is unfortunate.';
+  }
+}
+function checkAnswer9() {
+// Set the vaue of the underlined word
+partOfSpeech9 = 'adverb';
+
+// Get the user's input value
+userAnswer9 = document.getElementById('userAnswer9').value;
+
+// Compare the user's answer to the correct answer
+if (partOfSpeech9 == userAnswer9) {
+    // Log a message to make sure your logic works
+    console.log('They got it right!');
+
+    // Flash a message to the user letting them know it's right
+    message9 = document.getElementById('message9');
+    message9.innerHTML = 'That is the correct answer!';
+
+} else {
+    // Log a message to make sure your logic works
+    console.log('Incorrect');
+
+    // Flash a message to the user letting them know it's wrong
+    message9 = document.getElementById('message9');
+    message9.innerHTML = 'That is unfortunate.';
+  }
+}
+function checkAnswer10() {
+// Set the vaue of the underlined word
+partOfSpeech10 = 'preposition';
+
+// Get the user's input value
+userAnswer10 = document.getElementById('userAnswer10').value;
+
+// Compare the user's answer to the correct answer
+if (partOfSpeech10 == userAnswer10) {
+    // Log a message to make sure your logic works
+    console.log('They got it right!');
+
+    // Flash a message to the user letting them know it's right
+    message10 = document.getElementById('message10');
+    message10.innerHTML = 'That is the correct answer!';
+
+} else {
+    // Log a message to make sure your logic works
+    console.log('Incorrect');
+
+    // Flash a message to the user letting them know it's wrong
+    message10 = document.getElementById('message10');
+    message10.innerHTML = 'That is unfortunate.';
+  }
+}
